8e5ab6ebd1091e2f251c9aeca8f450727ea36c65
Kaitlyn Viers <Kaitlyn.viers@outlook.com>
2022-10-31 13:16:47 -0500

    Add project files.

---
+++ CodingEvents/Controllers/EventController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CodingEvents.Controllers
+{
+    
+    public class EventsController : Controller
+    {
+        static private Dictionary<string,string> Events = new Dictionary<string, string>();
+        [HttpGet]
+        public IActionResult Index()
+        {
+           /* Events.Add("Dan Howell");
+            Events.Add("Taylor Swift");
+            Events.Add("Graduation"); */
+           // New event method will let the user add events to the list.
+            ViewBag.events = Events;
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Events/Add")]
+        public IActionResult NewEvent(string name, string desc)
+        {
+            Events.Add(name, desc);
+            return Redirect("/Events");
+        }
+    }
+}
